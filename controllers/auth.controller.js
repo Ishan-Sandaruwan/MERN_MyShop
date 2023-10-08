23abf6558a53822466288254eb3aa01390a057bd
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-08 20:08:15 +0530

    signin function created successfully

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
     const { username, password, email, mobile } = req.body;
     const hashPass = bcrypt.hashSync(password, 10);
-    const newUser = new User({ username,password: hashPass, email, mobile });
+    const newUser = new User({ username, password: hashPass, email, mobile });
     await newUser.save();
     res.status(201).json({
       success: true,
@@ -17,6 +18,20 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export const signin = (req, res) => {
-  console.log("signin also working");
+export const signin = async (req, res , next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPass = bcrypt.compareSync(password, validUser.password);
+    if (!validPass) return next(errorHandler(401, "invalid password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,7 @@ const userShema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
